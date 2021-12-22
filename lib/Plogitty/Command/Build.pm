bd73e3b1945f00d5c35784435fe47c4c3772c751
Kaitlyn Parkhurst <symkat@symkat.com>
2021-12-22 11:05:43 -0800

    RSS generation support.

--- lib/Plogitty/Command/Build.pm
+++ lib/Plogitty/Command/Build.pm
@@ -6,6 +6,9 @@ use File::Basename qw( fileparse dirname );
 use File::Slurper qw( write_text );
 use File::Path qw( make_path remove_tree );
 use File::Copy::Recursive qw(dircopy);
+use XML::RSS;
+use DateTime;
+use CPAN::Meta::YAML;
 
 setup build => (
     desc => 'Create Build Directory',
@@ -35,6 +38,66 @@ define html => (
         # Give the user back the fully rendered markdown.
         $c->stash->{text} = $c->stash->{markdown_rendered};
     },
+);
+
+define rss => (
+    code => sub {
+        my ( $self, $c ) = @_;
+
+        my $plan = $self->run( qw( /quiet build:plan ) );
+
+        my @links;
+        foreach my $file ( @{$plan->stash->{files}} ) {
+            my $metadata = $c->model("Markdown")->metadata_from_file($file->[-1]);
+
+            next if $metadata->{index} and $metadata->{index} eq 'no';
+
+            push @links, { %{$metadata}, filename => $file->[0]  };
+        }
+
+        @links = sort { $b->{weight} <=> $a->{weight} } @links;
+
+        open my $lf, "<", 'config.yml'
+            or die "Failed to read config.yml: $!";
+        my $config_content = do { local $/; <$lf> };
+        close $lf;
+
+        my $data = CPAN::Meta::YAML->read_string( $config_content );
+
+
+        my $build_date = DateTime->now->strftime("%a, %d %b %Y %H:%M:%S %z");
+        my $rss = XML::RSS->new( version => '2.0' );
+        $rss->channel(
+            title         => $data->[0]{title},
+            link          => $data->[0]{link},
+            language      => $data->[0]{language},
+            description   => $data->[0]{description},
+            pubDate       => $build_date,
+            lastBuildDate => $build_date,
+        );
+
+        foreach my $link ( @links ) {
+
+            my $date;
+            if ( $link->{date} =~ /^(\d{4})-(\d{2})-(\d{2})$/ ) {
+                $date = DateTime->new(
+                    year  => $1,
+                    month => $2,
+                    day   => $3,
+                )->strftime( "%a, %d %b %Y %H:%M:%S %z" );
+            }
+
+            $rss->add_item(
+                title     => $link->{title},
+                permaLink => $data->[0]{link} . $link->{slug} . '.html',
+                link      => $data->[0]{link} . $link->{slug} . '.html',
+                pubDate   => $date,
+            );
+        }
+
+        # Give the user back the fully rendered RSS file.
+        $c->stash->{text} = $rss->as_string;
+    }
 );
 
 define index => (
@@ -106,10 +169,12 @@ define build => (
         }
         
         my $index = $self->run( qw( /quiet build:index ) );
+        write_text( "build/index.html", $index->stash->{index_page} );
+
+        my $rss = $self->run( qw( /quiet build:rss ) );
+        write_text( "build/rss.xml", $rss->stash->{text} );
 
         $self->run( qw( /quiet build:static ) );
-
-        write_text( "build/index.html", $index->stash->{index_page} );
 
         $c->stash->{text} = "Created build/\n";
     }
